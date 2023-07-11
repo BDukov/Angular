b8259f70c9d08a08c95c8062b6b8a994a928daba
Borislav Dukov <borislav.dukov@icloud.com>
2023-07-11 22:26:08 +0300

    diectives and  forms

--- 07.Forms/directives-and-forms/src/app/app.module.ts
+++ 07.Forms/directives-and-forms/src/app/app.module.ts
@@ -4,17 +4,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HighlightOnMoveDirective } from './highlight-on-move.directive';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { MyRouterLinkDirective } from './my-router-link.directive';
+import { MyStructuralDirective } from './my-structural.directive';
+import { PlaygroundComponent } from './playground/playground.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaxCountDirective } from './max-count.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HighlightOnMoveDirective
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    MyRouterLinkDirective,
+    HighlightOnMoveDirective,
+    MyStructuralDirective,
+    PlaygroundComponent,
+    MaxCountDirective,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, // Template Driven Form
+    ReactiveFormsModule, // Reactive Forms approach
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
